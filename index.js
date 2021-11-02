6872df13b40fa79c4483ffa2c17146d126002d84
Cleberson <clebersonjosea@gmail.com>
2021-11-02 10:42:05 -0300

    Configurando app

--- index.js
+++ index.js
@@ -7,9 +7,12 @@
 // TODO: Delete atendimento;
 
 import express from 'express';
+import cors from 'cors';
+import routes from './src/routes.js';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Server online'));
-
-app.listen(3000, () => console.log('Server online'));
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(3333, () => console.log("ta on"));
